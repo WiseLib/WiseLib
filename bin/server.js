0f2a3f0e566b33ffeee5445e10c17dff6358b5a3
Arno <arno.moonens@gmail.com>
2014-11-25 18:23:16 +0100

    Simple test

--- bin/server.js
+++ bin/server.js
@@ -7,3 +7,25 @@
  * Copyright (c) 2014 WiseLib
  * Licensed under the GPL-2.0 license.
  */
+
+
+ // set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// log every request to the console
+app.use(bodyParser.urlencoded({'extended': 'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
+app.listen(8080);
+console.log('Listening on port 8080');
+
+
+app.get('/api/test', function (req, res) {
+	res.send('boo');
+});
