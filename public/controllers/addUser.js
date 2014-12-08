cc84a6ecf042112e4b26e1f8956f78919ffc20f7
Arno <arno.moonens@gmail.com>
2014-12-08 21:06:45 +0100

    Moved client code to server and changed server.js accordingly to load index.html

---
+++ public/controllers/addUser.js
@@ -0,0 +1,46 @@
+var addUser = angular.module('addUser', []);
+
+addUser.controller('manageUserController', function($scope, $http) {
+  'use strict';
+  $scope.userForm = {};
+  $scope.persons = [];
+  $scope.userForm.radioChecked = false;
+
+  //feedback after clicking the submit button
+  $scope.successMessage = '';
+  $scope.errorMessage = '';
+
+  $scope.$watch('userForm.firstName', function() {$scope.searchPersons();});
+  $scope.$watch('userForm.lastName', function() {$scope.searchPersons();});
+
+  /**
+   * Sends a request (if necessary) to the server to search for a person
+   * @return {None}
+   */
+  $scope.searchPersons = function() {
+    $scope.userForm.radioChecked = false; //Deselect radio button
+    $scope.persons = []; //Reset found persons list
+    if(!$scope.userForm.firstName || !$scope.userForm.lastName || $scope.userForm.firstName.length < 3 || $scope.userForm.lastName.length < 3) {return;} //Don't search if first or last name is too short
+    console.log('Will search for persons');
+    $http.post('/api/person/search.json', {'firstName': $scope.userForm.firstName, 'lastName': $scope.userForm.lastName})
+      .success(function (data) {
+        console.log('received ' + JSON.stringify(data));
+        $scope.persons = data;
+      });
+  };
+
+/**
+ * Sends a request to the server to register a user using form input
+ * @return {None}
+ */
+  $scope.createUser = function() {
+    $http.post('/api/person', $scope.userForm)
+    .success(function (data) {
+      $scope.successMessage = 'User succesfully added';
+    })
+    .error(function (data) {
+      $scope.errorMessage = 'Error: ' + data;
+    });
+  };
+
+});
