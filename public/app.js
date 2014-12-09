96b913b031be6d24bf07af1a3dd3cbd8e87ca294
Arno <arno.moonens@gmail.com>
2014-12-09 14:15:56 +0100

    Removed example code and fixed indentation

--- public/app.js
+++ public/app.js
@@ -1,43 +1,24 @@
 var app = angular.module('client', ['ngRoute', 'addUser']);
-
-
 /**
  * Configure the Routes
  */
-app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
-  'use strict';
-  $routeProvider
+app.config(['$routeProvider', function ($routeProvider) {
+    'use strict';
+    $routeProvider
     // Home
     .when('/', {templateUrl: 'views/start.html', controller: 'mainController'})
     .when('/adduser', {templateUrl: 'views/register.html', controller: 'manageUserController'})
     .otherwise({redirectTo: '/'});
-
-  
 }]);
 
-
-
-app.controller('mainController', function($scope, $http) {
-  'use strict';
-  $http.get('/api/test')
-        .success(function(data) {
-          console.log('got data: ' + data);
-          $scope.response = data;
+app.controller('mainController', function ($scope, $http) {
+    'use strict';
+    $http.get('/api/test')
+        .success(function (data) {
+            console.log('got data: ' + data);
+            $scope.response = data;
         })
-        .error(function(data) {
+        .error(function (data) {
             console.log('Error: ' + data);
-          });
+        });
 });
-
-
-/*.config(function ($routeProvider) {
-  'use strict';
-  $routeProvider
-    .when('/todo', {
-      controller: 'TodoCtrl',
-      templateUrl: '/client/todo/todo.html'
-    })
-    .otherwise({
-      redirectTo: '/todo'
-    });
-});*/
