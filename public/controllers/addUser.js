38934392404c1856cf94988bf73f772e3872a4c8
Arno <arno.moonens@gmail.com>
2014-12-12 09:17:57 +0100

    Fixed code according to conventions and added 'use strict' statement.

--- public/controllers/addUser.js
+++ public/controllers/addUser.js
@@ -1,45 +1,45 @@
 var addUser = angular.module('addUser', []);
 
-addUser.controller('manageUserController', function($scope, $http) {
-  'use strict';
-  $scope.userForm = {};
-  $scope.persons = [];
-  $scope.userForm.radioChecked = false;
+addUser.controller('manageUserController', function ($scope, $http) {
+    'use strict';
+    $scope.userForm = {};
+    $scope.persons = [];
+    $scope.userForm.radioChecked = false;
 
-  //feedback after clicking the submit button
-  $scope.successMessage = '';
-  $scope.errorMessage = '';
+    //feedback after clicking the submit button
+    $scope.successMessage = '';
+    $scope.errorMessage = '';
 
-  $scope.$watch('userForm.firstName', function() {$scope.searchPersons();});
-  $scope.$watch('userForm.lastName', function() {$scope.searchPersons();});
+    $scope.$watch('userForm.firstName', function () {$scope.searchPersons(); });
+    $scope.$watch('userForm.lastName', function () {$scope.searchPersons(); });
+
+    /**
+     * Sends a request (if necessary) to the server to search for a person
+     * @return {None}
+     */
+    $scope.searchPersons = function () {
+        $scope.userForm.radioChecked = false; //Deselect radio button
+        $scope.persons = []; //Reset found persons list
+        if (!$scope.userForm.firstName || !$scope.userForm.lastName || $scope.userForm.firstName.length < 3 || $scope.userForm.lastName.length < 3) {return; } //Don't search if first or last name is too short
+        $http.get('/persons.json' + '?firstName=' + $scope.userForm.firstName + '&lastName=' + $scope.userForm.lastName)
+          .then(function (response) {
+            console.log('received ' + JSON.stringify(response.data.persons));
+            $scope.persons = response.data.persons;
+        });
+    };
 
   /**
-   * Sends a request (if necessary) to the server to search for a person
+   * Sends a request to the server to register a user using form input
    * @return {None}
    */
-  $scope.searchPersons = function() {
-    $scope.userForm.radioChecked = false; //Deselect radio button
-    $scope.persons = []; //Reset found persons list
-    if(!$scope.userForm.firstName || !$scope.userForm.lastName || $scope.userForm.firstName.length < 3 || $scope.userForm.lastName.length < 3) {return;} //Don't search if first or last name is too short
-    $http.get('/persons.json' + '?firstName=' + $scope.userForm.firstName + '&lastName=' + $scope.userForm.lastName)
-      .then(function (response) {
-        console.log('received ' + JSON.stringify(response.data.persons));
-        $scope.persons = response.data.persons;
+    $scope.createUser = function () {
+        $http.post('persons.json', $scope.userForm)
+          .success(function (data) {
+            $scope.successMessage = 'User succesfully added';
+        })
+          .error(function (data) {
+            $scope.errorMessage = 'Error: ' + data;
       });
-  };
-
-/**
- * Sends a request to the server to register a user using form input
- * @return {None}
- */
-  $scope.createUser = function() {
-    $http.post('persons.json', $scope.userForm)
-    .success(function (data) {
-      $scope.successMessage = 'User succesfully added';
-    })
-    .error(function (data) {
-      $scope.errorMessage = 'Error: ' + data;
-    });
-  };
+    };
 
 });
