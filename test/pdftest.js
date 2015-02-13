7688eb7e07d90d8289840f80b845f28848a83449
yverschu <yverschu@vub.ac.be>
2015-02-13 13:19:20 +0100

    Pdf analyse + tests

---
+++ test/pdftest.js
@@ -0,0 +1,16 @@
+var pdf = require ('../lib/pdf_analyse.js');
+var pdflist = ['../pdfs/test2.pdf','../pdfs/test3.pdf','../pdfs/test4.pdf','../pdfs/test5.pdf','../pdfs/test6.pdf','../pdfs/test7.pdf','../pdfs/test8.pdf'];
+var options = {
+  type: 'text'  // extract the actual text in the pdf file
+};
+
+
+for (var i = 0; i < pdflist.length; i++) {
+	var path =  pdflist[i];
+	pdf.analysePdf(path,options,function(result){
+		console.log('\n' + "Pdf: " + result[2]);
+		console.log("Title: " + result[0]);
+		console.log('\n' + "Authors:" + result[1]);
+	});
+};
+
