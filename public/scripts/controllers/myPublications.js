800594bf7e84b3244c781e86a9c46d9167fe930b
Arno <arno.moonens@gmail.com>
2015-02-27 15:47:39 +0100

    'my publications' page now shows list of publications fetched from server

--- public/scripts/controllers/myPublications.js
+++ public/scripts/controllers/myPublications.js
@@ -1,6 +1,18 @@
 'use strict';
 var myPublications = angular.module('myPublications', []);
-myPublications.controller('myPublicationsController', function($scope, Page) {
+myPublications.controller('myPublicationsController', function($scope, $window, Page, Publication) {
 	Page.setTitle('My publications');
-	$scope.publications = [];
+
+	var token = $window.sessionStorage.token;
+	var userId = parseInt(atob(token.split('.')[1]), 10);
+	Publication.query({id: userId}, function(data) {
+		console.log('got data!');
+		console.log(data);
+		$scope.publications = data.publications;
+	}, function(error) {
+		console.warn('error:' + error);
+	});
+
+	// $scope.publications = [{title: 'Test', publishedInYear: 2014, nrOfPages: 23},
+	// 					   {title: 'Andere test', publishedInYear: 2015, nrOfPages: 17}];
 });
