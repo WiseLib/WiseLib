6f11b9351d1f9baf26e32c4b2a132950695985fd
Sam Vervaeck <sam@vervaeck.be>
2015-02-28 14:45:06 +0100

    Rename manageUserController to registerUserController

--- public/app.js
+++ public/app.js
@@ -15,14 +15,14 @@ angular.module('client', ['ngMaterial', 'ngRoute', 'addUser', 'addPublication', 
         controller: 'mainController'})
     .when('/register', {
         templateUrl: 'views/register.html', 
-        controller: 'manageUserController'
+        controller: 'registerUserController'
     })
     .when('/upload', {
-        templateUrl: 'views/upload.html',
+        templateUrl: 'views/upload.html', 
         controller: 'uploadPublicationController'
     })
     .when('/login', {
-        templateUrl: 'views/loginUser.html',
+        templateUrl: 'views/loginUser.html', 
         controller: 'loginUserController'
     })
     .when('/mypublications', {
