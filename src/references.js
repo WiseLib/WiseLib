81d2d17ebf4306ec3074797fcde7d55509f76407
wovari <wovari@hotmail.com>
2015-03-23 10:30:49 +0100

    references bezig

---
+++ src/references.js
@@ -0,0 +1,36 @@
+function link(req,res){
+    var references = req.references;
+    var resultId = [];
+    //Alle references moeten afgegaan worden en gezocht worden in de database
+    for(index = 0; index < references.length; index++){
+        var reference = references[index];
+        var result[index] = searchDB(reference);
+    }
+}
+
+function searchDB(reference){
+    var title =  reference.entryTag.title;
+    var result = DBManager.get({title: title}, publicationRepr, function(results){
+        if(data.length < 1){
+            var newId;
+            DBManager.post(reference, publicationRepr, function(id){
+                newId = id;
+            })
+            //TODO publication_with_unknown_person tabel vullen met de nieuwe publication
+            /*
+            * for(index = 0; index < reference.authors.length; index++){
+            *       DBManager.post()
+            *    }
+            */
+        }
+        if(data.length = 1){
+            return results.id;
+        }
+        if(data.length < 1){
+            //TODO publication_with_unknown_person tabel vullen met nieuwe publication
+            // eventueel bestaande users contacteren mbv een notification dat met person validation
+        }
+    });
+
+    //TODO finish function
+}
