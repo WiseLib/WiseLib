6f748b61e480a67d31b8085ac317ffefac00af15
Arno <arno.moonens@gmail.com>
2015-03-28 21:03:46 +0100

    Added editors and uploader person to 'publication' page

--- public/scripts/controllers/publication.js
+++ public/scripts/controllers/publication.js
@@ -1,24 +1,48 @@
 'use strict';
 var module = angular.module('publication');
 
-module.controller('publicationController', function($scope, $window, $routeParams, Page, Publication, Person) {
+module.controller('publicationController', function($scope, $window, $routeParams, Page, Publication, Person, User) {
     Page.setTitle('Publication');
     $scope.publication = undefined;
+    $scope.persons = {};
     $scope.authors = [];
+    $scope.editors = [];
     $scope.publications = [];
 
     Publication.get({id: $routeParams.id}, function(pub) {
         console.log(pub);
         function getPerson(id) {
             Person.get({id: id}, function(person) {
-                $scope.authors.push(person);
+                $scope.persons[id] = person;
             }, function(data) {
                 console.log('error: ' + data.error);
             });
         }
+
+        User.get({id: pub.uploader}, function(user) {
+            console.log('got user! ' + JSON.stringify(user));
+            $scope.uploaderPersonId = user.person;
+            if(!$scope.persons[user.person]) {
+                getPerson(user.person);
+            }
+        }, function(data) {
+            console.log('error: ' + data.error);
+        });
         $scope.publication = pub;
         for (var i = pub.authors.length - 1; i >= 0; i--) {
-            getPerson(pub.authors[i].id);
+            var id = pub.authors[i].id;
+            $scope.authors.push(id);
+            if(!$scope.persons[id]) {
+                getPerson(id);
+            }
+        }
+
+        for (i = pub.editors.length - 1; i >= 0; i--) {
+            var id = pub.editors[i].id;
+            $scope.editors.push(id);
+            if(!$scope.persons[id]) {
+                getPerson(id);
+            }
         }
 
         function getPublication(id) {
