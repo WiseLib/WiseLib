7625deaf1fdd3f4116a43617a18586cb4cfce45f
yverschu <yverschu@vub.ac.be>
2015-04-13 18:06:14 +0200

    New Ranking formulas

--- src/ranking.js
+++ src/ranking.js
@@ -1,80 +1,93 @@
 'use strict';
 var DBManager = require('./dbmanager.js');
 var linker = require('./linker.js');
+var Promise = require('bluebird');
 
 function calculateRank(jsonObj, type, callback) { //type = linker representation
+
+	/*
+		Journal/Conference = ligt vast (in database)
+		Persoon = combinatie van aantal gepubliceerde publicaties en publicaties per jaar 
+		Publicatie = combinatie van rank van auteurs, aantal citaties en rank van Journal/Conference
+	 */
+
+	var PublicationRank = function(subRank) {
+
+		var numberOfCitations = jsonObj.referencedPublications.length;
+
+		var authorRanks = [];
+
+		var counter=0;
+		jsonObj.authors.forEach(function(author) {
+				DBManager.get({id: author.id}, linker.personRepr,
+					function(result) {
+						calculateRank(result[0], linker.personRepr, function(rank) {
+								authorRanks.push(rank);
+								++counter;
+								if (counter === jsonObj.authors.length) {
+									var averageAuthorRank = 0;
+									authorRanks.forEach(function(authorRank) {
+										averageAuthorRank += authorRank
+									});
+									averageAuthorRank = averageAuthorRank / authorRanks.length;
+									callback(numberOfCitations * subRank * averageAuthorRank); //no citations : very bad --> rank = 0 
+								}
+
+							})
+					});
+			})
+	}
 
 	if (type === linker.proceedingPublicationRepr) {
 		var rank;
 		var proceedingId = jsonObj.proceeding;
 		if (proceedingId === undefined) callback(undefined);
-		DBManager.get({
-			id: proceedingId
+		DBManager.get({id: proceedingId
 		}, linker.proceedingRepr, function(result) {
+
 			rank = result[0].rank;
-			callback(rank);
+			PublicationRank(rank);
 		})
 	} else if (type === linker.journalPublicationRepr) {
 		var rank;
 		var journalId = jsonObj.journal;
 		if (journalId === undefined) callback(undefined);
-		DBManager.get({
-			id: journalId
+		DBManager.get({id: journalId
 		}, linker.journalRepr, function(result) {
 			rank = result[0].rank;
-			callback(rank);
+			PublicationRank(rank);
 		})
 
-	} else if (type === linker.personRepr) {
+	} else if (type === linker.personRepr) { //gemiddelde aantal publicaties (per jaar) * (1 + publicaties dit jaar)
 		var publications = jsonObj.publications;
-		var ranks = new Array(jsonObj.length);
 
-		var compoundRank = function() {
-			var rank = 0;
-			ranks.forEach(function(pubRank) {
-				rank += pubRank;
+		var numberOfPublications = publications.length;
+
+		var years = new Array();
+
+		var counter = 0
+		publications.map(function(publication) {
+			DBManager.get({id: publication.id
+			}, linker.publicationRepr, function(result) {
+				counter++;
+				years.push(result[0].year);
+
+				if (counter === numberOfPublications) {
+					years.sort();
+
+					var yearsOfWriting = years[years.length - 1] - years[0];
+					var date = new Date();
+					var thisYearIndex = years.indexOf(date.getFullYear());
+					var publicationsThisYear = years.length - thisYearIndex;
+					var rank = (numberOfPublications / (yearsOfWriting + 1)) * (1 + publicationsThisYear); // years + 1 because of terrible test data --> /0 errors
+
+					callback(rank);
+				}
 			})
+		})
 
-			rank /= ranks.length;
-			callback(rank);
-		}
 
-		var length = publications.length
-		for (var i = 0; i < publications.length; i++) {
-			(function(i) {
-				var publicationid = publications[i].id;
-				var pubresult;
-				var type;
-				DBManager.get({id: publicationid}, linker.journalPublicationRepr, function(result) {
-					if (result.length == 0) { //not a journal publication
-						DBManager.get({id: publicationid}, linker.proceedingPublicationRepr, function(result) {
-							if (result.length == 0) { //not a proceeding publication --> error: rank = 0
-								ranks[i] = 0;
-								--length;
-								if (length == 0) compoundRank();
-							}
-							else {
-								pubresult = result[0]; //publication is proceeding publication
-								type = linker.proceedingPublicationRepr;
-								calculateRank(pubresult, type, function(rank) { //calculate its rank
-									ranks[i] = rank;
-									--length;
-									if (length == 0) compoundRank();
-								})
-							}
-						})
-					} else { //publication is journal publication
-						pubresult = result[0];
-						type = linker.journalPublicationRepr;
-						calculateRank(pubresult, type, function(rank) {
-							ranks[i] = rank;
-							--length;
-							if (length == 0) compoundRank();
-						})
-					}
-				})
-			}(i));
-		};
+
 	} else throw new Error('Wrong type for ranking:' + type)
 }
 
