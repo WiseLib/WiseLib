1938103aa971b7efe6d61ed5c95ec9fbe525b24a
yverschu <yverschu@vub.ac.be>
2015-04-15 18:16:47 +0200

    search journal/proceedings by name

--- public/scripts/services/journal.js
+++ public/scripts/services/journal.js
@@ -5,13 +5,13 @@ angular.module('journal', [])
 .factory('Journal', function($resource) {
 	return $resource('/journals.json', {}, {
 		query: { method: 'GET', isArray: false },
-		search: { method: 'GET', url:'/journals.json/?q=:q',params: {q:'@q'}},
+		search: { method: 'GET', url:'/journals.json/?q=:q@name',params: {q:'@q'}},
 	});
 })
 
 .factory('Proceeding', function($resource) {
 	return $resource('/proceedings.json', {}, {
 		query: { method: 'GET', isArray: false },
-		search: { method: 'GET', url:'/proceedings.json/?q=:q',params: {q:'@q'}},
+		search: { method: 'GET', url:'/proceedings.json/?q=:q@name',params: {q:'@q'}},
 	});
 });
