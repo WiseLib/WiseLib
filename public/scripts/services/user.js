7d9c42221fc577e03620287ad9f550672383844d
Mathieu Reymond <mathieu.reymond@gmail.com>
2015-04-21 12:48:38 +0200

    fixed update profile bug

--- public/scripts/services/user.js
+++ public/scripts/services/user.js
@@ -4,7 +4,8 @@ angular.module('user')
 
 .factory('User', function($resource) {
     return $resource('/users/:id.json', {id:'@id'}, {
-        get: {method: 'GET'}
+        get: {method: 'GET'},
+        put: {method: 'PUT'}
     });
 })
 
